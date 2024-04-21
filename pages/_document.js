f1c15d67990fa0f0aeb2d218227d38d345dc3f8e
Jhonattas Ferreira <jfs120@gmail.com>
2024-04-21 10:54:37 -0300

    create blog link and adjust imports

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,19 @@
 import Head from "next/head";
 import styles from "./index.module.css";
 
-const index = () => (
+const Index = () => (
   <>
     <Head>
       <title>Jhonattas</title>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      ></link>
     </Head>
+
+    <header>
+      <nav className={styles.nav}>
+        <a href="#">Blog</a>
+      </nav>
+    </header>
 
     <main className={styles.container}>
       <section className={styles.title}>
@@ -45,15 +36,15 @@ const index = () => (
           target="_blank"
           href="https://www.linkedin.com/in/jhonattasferreira/"
         >
-          <i class="fa fa-linkedin-square"></i>
+          <i className="fa fa-linkedin-square"></i>
         </a>
 
         <a target="_blank" href="https://github.com/JhonattasFerreira">
-          <i class="fa fa-github-square"></i>
+          <i className="fa fa-github-square"></i>
         </a>
       </section>
     </main>
   </>
 );
 
-export default index;
+export default Index;
