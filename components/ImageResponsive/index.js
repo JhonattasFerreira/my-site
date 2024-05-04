d444de56abb6483f4fadd458e21d11fdbf6df193
Jhonattas Ferreira <jfs120@gmail.com>
2024-05-04 12:14:14 -0300

    adjust size image

--- components/ImageResponsive/index.js
+++ components/ImageResponsive/index.js
@@ -3,7 +3,7 @@ import styles from "./ImageResponsive.module.css";
 
 const ImageResponsive = ({ src, alt }) => (
   <div className={styles.imageResponsive}>
-    <Image src={src} alt={alt} sizes="35vw" />
+    <Image src={src} alt={alt} sizes="40vw" />
   </div>
 );
 
