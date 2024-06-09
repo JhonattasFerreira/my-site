171a68f1d8bc2cc00b023390f76ac7e39669e058
Jhonattas Ferreira <jfs120@gmail.com>
2024-06-09 12:18:51 -0300

    create variables and put on the listing

--- app/blog/en/page.js
+++ app/blog/en/page.js
@@ -1,24 +1,23 @@
 import getPostMetadata from "@/utils/getPostMetadata";
 import ListingPost from "@/components/ListingPost";
-
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import {
+  EN_LANGUAGE,
+  CONTENT_FOLDER,
+  LISTING_POSTS_METADATA_EN,
+} from "@/utils/variables";
 
-const LANGUAGE = "en";
-
-export const metadata = {
-  title: "All Posts | JhoCore",
-  description: "A blog by Jhonattas Ferreira",
-};
+export const metadata = LISTING_POSTS_METADATA_EN;
 
 const Blog = () => {
-  const postMetadata = getPostMetadata("content/posts", LANGUAGE);
+  const postMetadata = getPostMetadata(CONTENT_FOLDER, EN_LANGUAGE);
 
   return (
     <>
       <SpeedInsights />
       <Analytics />
-      <ListingPost postMetadata={postMetadata} language={LANGUAGE} />
+      <ListingPost postMetadata={postMetadata} language={EN_LANGUAGE} />
     </>
   );
 };
--- app/blog/pt-br/page.js
+++ app/blog/pt-br/page.js
@@ -2,22 +2,22 @@ import getPostMetadata from "@/utils/getPostMetadata";
 import ListingPost from "@/components/ListingPost";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import {
+  PT_BR_LANGUAGE,
+  CONTENT_FOLDER,
+  LISTING_POSTS_METADATA_PT_BR,
+} from "@/utils/variables";
 
-const LANGUAGE = "pt-br";
-
-export const metadata = {
-  title: "Todos os Posts | JhoCore",
-  description: "Um blog de Jhonattas Ferreira",
-};
+export const metadata = LISTING_POSTS_METADATA_PT_BR;
 
 const Blog = () => {
-  const postMetadata = getPostMetadata("content/posts", LANGUAGE);
+  const postMetadata = getPostMetadata(CONTENT_FOLDER, PT_BR_LANGUAGE);
 
   return (
     <>
       <SpeedInsights />
       <Analytics />
-      <ListingPost postMetadata={postMetadata} language={LANGUAGE} />
+      <ListingPost postMetadata={postMetadata} language={PT_BR_LANGUAGE} />
     </>
   );
 };
