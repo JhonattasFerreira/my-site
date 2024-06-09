82e479d7c8cf08489a7a1221fa2cb943ab731872
Jhonattas Ferreira <jfs120@gmail.com>
2024-06-09 13:08:17 -0300

    refactor post page

--- app/blog/en/[slug]/page.js
+++ app/blog/en/[slug]/page.js
@@ -1,61 +1,39 @@
-import fs from "fs";
-import matter from "gray-matter";
 import getPostMetadata from "@/utils/getPostMetadata";
 import LayoutPost from "@/components/LayoutPost";
 import transformString from "@/utils/transformString";
-
-function getPostContent(slug) {
-  const folder = "content/posts";
-
-  const postFolders = fs.readdirSync(folder + "/");
-
-  let filename;
-  let folderName;
-  let oppositeUrl;
-
-  for (const postFolder of postFolders) {
-    const files = fs.readdirSync(`${folder}/${postFolder}/`);
-    filename = files.find((file) => file.includes(slug));
-    if (filename) {
-      folderName = postFolder;
-      oppositeUrl = files.find((item) => item !== filename);
-      break;
-    }
-  }
-
-  const file = folder + `/${folderName}/${filename}`;
-
-  const content = fs.readFileSync(file, "utf8");
-
-  const matterResult = matter(content);
-  return {
-    data: matterResult.data,
-    content: matterResult.content,
-    oppositeUrl: `${oppositeUrl.replace(".pt-br.md", "")}`,
-  };
-}
+import {
+  CONTENT_FOLDER,
+  EN_LANGUAGE,
+  FILENAME_END_PT_BR,
+  TITLE_METADATA_POST_SUFIX,
+} from "@/utils/constants";
+import getPostContent from "@/utils/getPostContent";
 
 export const generateStaticParams = async () => {
-  const posts = getPostMetadata("content/posts", "en");
+  const posts = getPostMetadata(CONTENT_FOLDER, EN_LANGUAGE);
+
   return posts.map((post) => ({ slug: post.slug }));
 };
 
 export async function generateMetadata({ params, searchParams }) {
   return {
-    title: `${transformString(params?.slug)} | JhoCore`,
+    title: `${transformString(params?.slug)}${TITLE_METADATA_POST_SUFIX}`,
   };
 }
 
 const Post = (props) => {
   const slug = props.params.slug;
-  const { data, content, oppositeUrl } = getPostContent(slug);
+  const { data, content, oppositeUrl } = getPostContent(
+    slug,
+    FILENAME_END_PT_BR
+  );
 
   return (
     <LayoutPost
       frontmatter={data}
       content={content}
       slug={slug}
-      language={"en"}
+      language={EN_LANGUAGE}
       oppositeUrl={oppositeUrl}
     />
   );
--- app/blog/pt-br/[slug]/page.js
+++ app/blog/pt-br/[slug]/page.js
@@ -1,62 +1,36 @@
-import fs from "fs";
-import matter from "gray-matter";
 import getPostMetadata from "@/utils/getPostMetadata";
-import transformString from "@/utils/transformString";
-
 import LayoutPost from "@/components/LayoutPost";
-
-function getPostContent(slug) {
-  const folder = "content/posts";
-
-  const postFolders = fs.readdirSync(folder + "/");
-
-  let filename;
-  let folderName;
-  let oppositeUrl;
-
-  for (const postFolder of postFolders) {
-    const files = fs.readdirSync(`${folder}/${postFolder}/`);
-    filename = files.find((file) => file.includes(slug));
-    if (filename) {
-      folderName = postFolder;
-      oppositeUrl = files.find((item) => item !== filename);
-      break;
-    }
-  }
-
-  const file = folder + `/${folderName}/${filename}`;
-
-  const content = fs.readFileSync(file, "utf8");
-
-  const matterResult = matter(content);
-  return {
-    data: matterResult.data,
-    content: matterResult.content,
-    oppositeUrl: `${oppositeUrl.replace(".en.md", "")}`,
-  };
-}
+import transformString from "@/utils/transformString";
+import {
+  CONTENT_FOLDER,
+  PT_BR_LANGUAGE,
+  FILENAME_END_EN,
+  TITLE_METADATA_POST_SUFIX,
+} from "@/utils/constants";
+import getPostContent from "@/utils/getPostContent";
 
 export const generateStaticParams = async () => {
-  const posts = getPostMetadata("content/posts", "pt-br");
+  const posts = getPostMetadata(CONTENT_FOLDER, PT_BR_LANGUAGE);
+
   return posts.map((post) => ({ slug: `${post.slug}` }));
 };
 
 export async function generateMetadata({ params, searchParams }) {
   return {
-    title: `${transformString(params?.slug)} | JhoCore`,
+    title: `${transformString(params?.slug)}${TITLE_METADATA_POST_SUFIX}`,
   };
 }
 
 const Post = (props) => {
   const slug = props.params.slug;
-  const { data, content, oppositeUrl } = getPostContent(slug);
+  const { data, content, oppositeUrl } = getPostContent(slug, FILENAME_END_EN);
 
   return (
     <LayoutPost
       frontmatter={data}
       content={content}
       slug={slug}
-      language={"pt-br"}
+      language={PT_BR_LANGUAGE}
       oppositeUrl={oppositeUrl}
     />
   );
