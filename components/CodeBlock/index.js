2cb7649c351fbb2587ba1fc9be9a889ff8ec1c73
JhonattasFerreira <jfs120@gmail.com>
2026-05-07 22:02:58 -0300

    perf: remove ssr: false from CodeBlock to fix code indexing
    Code blocks were not included in static HTML, preventing search engines
    from indexing technical content in blog posts.

--- components/CodeBlock/index.js
+++ components/CodeBlock/index.js
@@ -1,8 +1,6 @@
-"use client";
-
 import dynamic from "next/dynamic";
 
-const SyntaxHighlighterBlock = dynamic(() => import("./SyntaxHighlighterBlock"), { ssr: false });
+const SyntaxHighlighterBlock = dynamic(() => import("./SyntaxHighlighterBlock"));
 
 const CodeBlock = ({ children, className }) => {
   if (!className) {
